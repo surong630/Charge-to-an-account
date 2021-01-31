339d772e5fe072aaf83133a0a0ca467ebd2128d7
insistsu <947737976@qq.com>
2021-01-31 18:32:30 +0800

    add 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,17 @@ import VueRouter, { RouteConfig } from 'vue-router'
 import Money from '@/views/Money.vue'
 import Label from '@/views/Label.vue'
 import Statistics from '@/views/Statistics.vue'
+import NotFound from '@/views/NotFound.vue'
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
     redirect: '/money'
+  },
+  {
+    path: '*',
+    component: NotFound
   },
   {
     path: '/money',
---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    404
+    <router-link to="/money">返回首页</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
