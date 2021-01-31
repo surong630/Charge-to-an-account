a0a9a53b37e18405654644090405f094113823ce
insistsu <947737976@qq.com>
2021-01-31 19:06:37 +0800

    add svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,34 @@
 <template>
   <div id="nav">
-    <router-link to="/money">Money</router-link>
-    <router-link to="/Label">Label</router-link>
-    <router-link to="/Statistics">Statistics</router-link>
+    <router-link to="/money">
+    <svg>
+      <use xlink:href="#money"></use>
+    </svg>
+    Money
+    </router-link>
+    <router-link to="/Label">
+    <svg>
+      <use xlink:href="#label"></use>
+    </svg>
+    Label
+    </router-link>
+    <router-link to="/Statistics">
+        <svg>
+      <use xlink:href="#statistics"></use>
+    </svg>
+    Statistics</router-link>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => {
+  requireContext.keys().forEach(requireContext);
+}
+try{
+  importAll(require.context('../assets/iconfont', true, /\.svg$/))
+}catch(err){
+  console.log(err);
+}
 export default {};
 </script>
 
